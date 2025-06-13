b1a056fab2e4c283fd67121287770d3a1725aff1
stmata <108939392+stmata@users.noreply.github.com>
2025-06-12 21:54:56 -0400

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -18,7 +18,8 @@
 //   if (!res.ok) throw new Error("Export failed");
 //   return res;
 // };
-const BASE_URL = import.meta.env.VITE_APP_BASE_URL.replace(/\/$/, ""); // Supprime le slash final
+// const BASE_URL = import.meta.env.VITE_APP_BASE_URL.replace(/\/$/, ""); // Supprime le slash final
+const BASE_URL = (window._env_?.VITE_APP_BASE_URL || import.meta.env.VITE_APP_BASE_URL).replace(/\/$/, "");
 
 export const postFormData = async (endpoint, formData) => {
   console.log(formData)
